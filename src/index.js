cc2a8049d4d0c9409ce477f7427c69893553a594
Mark Ryall <mark@ryall.name>
2016-07-09 20:16:40 +1000

    change entry point to es5 exports

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import create from './create';
 import cellStates from './cellStates';
 import gameStates from './gameStates';
 
-export default { create, cellStates, gameStates };
+module.exports = {
+  create,
+  cellStates,
+  gameStates
+};
